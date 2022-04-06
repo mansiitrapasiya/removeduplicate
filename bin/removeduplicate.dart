12c7ba8c4b1ba0b9ad33c94a91b2f6f87101e41b
mansiitrapasiya <trapasiyamansi304@gmail.com>
2022-04-06 11:05:27 +0530

    change code

--- bin/removeduplicate.dart
+++ bin/removeduplicate.dart
@@ -1,7 +1,12 @@
 import 'dart:io';
 
 void main() {
-  List<String> duplicate = ["india", "usa", "australia", "usa"];
-  final uniqueNumbers = duplicate.toSet().toList();
+  List<String> duplicate = [
+    "india",
+    "usa",
+    "australia",
+    "usa",
+  ];
+  final uniqueNumbers = duplicate.toList();
   print("uniqueNumbers: \n $uniqueNumbers");
 }
